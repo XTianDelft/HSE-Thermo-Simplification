ce3557582bc8bfdf93fbcbba6c0610b68136466a
Wouter de Ruiter <w.deruiter@student.tudelft.nl>
2025-11-24 17:22:10 +0100

    add profile reader

---
+++ read_profile.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel("profiles/profile-test.xlsx")
+heat_demand = df["Ruimteverwarming_W"]
+time = df["time"]
+
+print(f"max hourly demand: {heat_demand.max() / 1e3:.2f} kW")
+
+plt.plot(heat_demand)
+plt.show()
