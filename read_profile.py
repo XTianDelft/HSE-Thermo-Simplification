75ab49412bef49bcd20875ce2df869b02339daae
Wouter de Ruiter <w.deruiter@student.tudelft.nl>
2025-11-26 12:10:37 +0100

    read montly peak/total heat load from profile

--- read_profile.py
+++ read_profile.py
@@ -2,13 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-R_b = 0.1125
+months = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+]
 
-df = pd.read_excel("profiles/profile-test.xlsx")
-heat_demand = df["Ruimteverwarming_W"]
-time = df["time"]
 
-print(f"max hourly demand: {heat_demand.max() / 1e3:.2f} kW")
+# returns the peak load per month (in kW) and the total load per month (in MWh)
+def read_monthly_profile(filename):
+    df = pd.read_excel(filename)
+    heat_demand = df["Ruimteverwarming_W"]
+    time = df["time"]
 
-plt.plot(heat_demand)
-plt.show()
+    print(df)
+
+    print(f"\nmax hourly demand: {heat_demand.max() / 1e3:.2f} kW")
+
+    hours_per_month = 8760 // 12
+    monthly = heat_demand.to_numpy().reshape((12, hours_per_month))
+
+    return monthly.max(axis=1) / 1e3, monthly.sum(axis=1) / 1e6
+
+
+read_monthly_profile("profiles/profile-test.xlsx")
