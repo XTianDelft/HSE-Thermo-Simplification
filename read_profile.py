27378a8d26e2e5637bf6f66405ff8bb78905f06a
Wouter de Ruiter <w.deruiter@student.tudelft.nl>
2025-11-26 12:30:51 +0100

    this makes more sense

--- read_profile.py
+++ read_profile.py
@@ -18,20 +18,16 @@ months = [
 ]
 
 
-# returns the peak load per month (in kW) and the total load per month (in MWh)
+# returns the baseload per month (in kWh/month) and the peak load per month (in kW/month)
 def read_monthly_profile(filename):
     df = pd.read_excel(filename)
     heat_demand = df["Ruimteverwarming_W"]
     time = df["time"]
 
-    print(df)
-
-    print(f"\nmax hourly demand: {heat_demand.max() / 1e3:.2f} kW")
-
     hours_per_month = 8760 // 12
     monthly = heat_demand.to_numpy().reshape((12, hours_per_month))
 
-    return monthly.max(axis=1) / 1e3, monthly.sum(axis=1) / 1e6
+    return monthly.sum(axis=1) / 1e3, monthly.max(axis=1) / 1e3
 
 
 read_monthly_profile("profiles/profile-test.xlsx")
