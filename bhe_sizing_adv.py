bba6431296974479557ae11919e1b6ff80acc01f
XTianDelft <x.tian-4@student.tudelft.nl>
2025-12-16 19:00:14 +0100

    Added Variables annotations

--- bhe_sizing_adv.py
+++ bhe_sizing_adv.py
@@ -6,16 +6,15 @@ from GHEtool import *
 
 from heat_profile import *
 
+#--------------------Initial Conditions----------------------------
 T_g = 10     # undisturbed ground temperature
 k_g = 2.8   # ground thermal conductivity
 Cp = 2.8e6  # ground volumetric heat capacity in J/m3K
 Rb = 0.1125
 r_b = 0.125 #Borehole radius
-
 # min and max fluid temperatures
 Tf_max = 16
 Tf_min = 0
-
 #Property Boundaries
 R_max = 30 #Max Radius in m
 H_max = 40 #Max depth in m
@@ -26,18 +25,6 @@ ground = GroundConstantTemperature(k_g, T_g, Cp)
 profile_fn = "profiles/profile-test.xlsx"
 heat_profile = read_hourly_profile(profile_fn)
 load = HourlyBuildingLoad(heat_profile/1e3)
-
-# with open('./HSE data/outputs/EnergyMeter_outputs.pkl', 'rb') as f:
-#     aggregated_df = pickle.load(f)['aggregated_df']
-# heat_profile_kW = aggregated_df['Power_KW'].to_numpy()[:8760]
-# is_extracting = heat_profile_kW >= 0
-# extraction_load = heat_profile_kW * is_extracting
-# injection_load = -(heat_profile_kW * ~is_extracting)
-# plt.plot(injection_load)
-# plt.show()
-# load = HourlyGeothermalLoad(extraction_load)
-
-
 
 borefield = Borefield(load=load)
 
@@ -60,23 +47,5 @@ borefield.set_borefield(gt_borefield)
 # borefield.create_custom_dataset(options={'method': 'similarities'})
 
 length = borefield.size(L4_sizing=True) #each borehole length
-
 print(f"The borehole length (with {Nb} boreholes) is: {length} m")
 print(f"{borefield.limiting_quadrant = }")
-
-# print imbalance
-print("The borefield imbalance is: ", borefield.load.imbalance,
-        "kWh/y. (A negative imbalance means the the field is heat extraction dominated so it cools down year after year.)")  # print imbalance
-
-Tf = borefield.results.Tf
-
-# plot temperature profile for the calculated borehole length
-borefield.print_temperature_profile(legend=True, plot_hourly=True)
-
-# plot temperature profile for a fixed borehole length
-# borefield.print_temperature_profile_fixed_length(length=75, legend=False)
-
-# print gives the array of monthly temperatures for peak cooling without showing the plot
-borefield.calculate_temperatures(length=90)
-# print("Result array for cooling peaks")
-# print(borefield.results.peak_injection)
